890ab5224c9ba79490cc9237f895d06bd728ff36
rondido <rppr001@naver.com>
2024-10-08 23:12:51 +0900

    20241008 수업 내용

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import './App.css'
 
 interface Movie {
   Poster: string
@@ -10,22 +11,28 @@ interface Movie {
 function App() {
   const [message, setMessage] = useState('Hello world!')
   const [movies, setMovies] = useState<Movie[]>([])
-  const [isRed, setIsRed] = useState(false)
+  const [isShow, setIsShow] = useState(false)
 
   function handleClick() {
-    setMessage('Good Moring') //재할당
-    fetchMovies()
+    setIsShow(!isShow)
+    //fetchMovies()
   }
 
   async function fetchMovies() {
     const res = await fetch('https://omdbapi.com?apikey=7035c60c&s=avengers')
     const data = await res.json()
-    setMovies(data.Search)
+    return data.Search
   }
+
+  console.log(true && 123 && '0')
 
   return (
     <>
-      <h1 className={isRed ? 'active' : ''}>{message}</h1>
+      {isShow && <h1>{message}</h1>}
+      <input
+        value={message}
+        onChange={e => setMessage(e.target.value)}
+      />
       <button onClick={handleClick}>클릭!</button>
       <ul>
         {movies.map(movie => (
