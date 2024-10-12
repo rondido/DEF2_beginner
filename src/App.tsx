57a7449414de80ba51480c374381fea7ad9697ed
rondido <rppr001@naver.com>
2024-10-12 15:18:27 +0900

    20241012 수업 내용

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import './App.css'
 
 interface Movie {
@@ -9,29 +9,44 @@ interface Movie {
 }
 
 function App() {
-  const [message, setMessage] = useState('Hello world!')
+  const [title, setTitle] = useState('')
   const [movies, setMovies] = useState<Movie[]>([])
   const [isShow, setIsShow] = useState(false)
 
-  function handleClick() {
-    setIsShow(!isShow)
-    //fetchMovies()
+  const inputEl = useRef<HTMLInputElement | null>(null)
+
+  async function handleClick() {
+    // setIsShow(!isShow)
+    const movies = await fetchMovies(title)
+    setMovies(movies)
   }
 
-  async function fetchMovies() {
-    const res = await fetch('https://omdbapi.com?apikey=7035c60c&s=avengers')
+  async function fetchMovies(title: string) {
+    const res = await fetch(`https://omdbapi.com?apikey=7035c60c&s=${title}`)
     const data = await res.json()
     return data.Search
   }
 
-  console.log(true && 123 && '0')
+  async function handleKeyDown(event: React.KeyboardEvent<HTMLInputElement>) {
+    // mac에서 cjk 즉 중국어 한국어 일본어를 사용하면 api나 log가 2번 호출되는 것을 방지할 수 있다.
+    if (event.key === 'Enter' && !event.nativeEvent.isComposing) {
+      const movies = await fetchMovies(title)
+      setMovies(movies)
+    }
+  }
+
+  useEffect(() => {
+    inputEl.current?.focus()
+  }, [])
 
   return (
     <>
-      {isShow && <h1>{message}</h1>}
+      {isShow && <h1>{title}</h1>}
       <input
-        value={message}
-        onChange={e => setMessage(e.target.value)}
+        value={title}
+        onChange={e => setTitle(e.target.value)}
+        onKeyDown={handleKeyDown}
+        ref={inputEl}
       />
       <button onClick={handleClick}>클릭!</button>
       <ul>
